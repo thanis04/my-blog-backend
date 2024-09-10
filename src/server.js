c23eea939916a31c80f18091c7e750505ff7a3e0
Thanis <mtmorion75@gmail.com>
2024-09-10 13:54:16 +0400

    mongodb database connected

---
+++ src/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.json(article);    
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.put('/api/articles/:name/upvotes', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article does\'t exists');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article does\'t exists');
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
